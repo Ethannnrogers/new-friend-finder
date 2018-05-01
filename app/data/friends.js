4872239d02587cb59363bba8428ae050c5d546ee
Ethan <erogers999@gmail.com>
2018-05-01 14:11:34 -0400

    update

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+var friendsData = [
+    {
+      name: "Jonah Hill",
+      photo: "https://www.google.com/search?q=wolf+of+wall+street+actors&rlz=1C5CHFA_enUS705US705&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiE3qO7vIvaAhXqYN8KHalZDDQQ_AUICigB&biw=1220&bih=657#imgrc=rml_ACBlhKR0qM:",
+      scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+    },
+    {
+      name: "Leonardo Decaprio",
+      photo:"https://www.google.com/search?q=wolf+of+wall+street+actors&rlz=1C5CHFA_enUS705US705&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiE3qO7vIvaAhXqYN8KHalZDDQQ_AUICigB&biw=1220&bih=657#imgrc=UgRNZX6PkAXw6M:",
+      scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    },
+    {
+      name: "Margot Robbie",
+      photo:"https://www.google.com/search?q=wolf+of+wall+street+actors&rlz=1C5CHFA_enUS705US705&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiE3qO7vIvaAhXqYN8KHalZDDQQ_AUICigB&biw=1220&bih=657#imgrc=TjnEMeMd8oSakM:",
+      scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+      name: "Matthew Mcconaughey",
+      photo:"http://bigpictures.club/resize.php?img=http://efekt-dieta.info/cdn/images/matthew-mcconaughey/matthew-mcconaughey-01.jpg",
+      scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    }
+  ];
+  
+
+  module.exports = friendsData;
